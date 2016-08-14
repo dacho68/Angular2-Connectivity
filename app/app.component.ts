d852f05ae4a839215ca5339ca7670e8b2ffc8208
dacho68 <dacho68@gmail.com>
2016-08-14 15:35:18 -0400

    add the exercise

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,7 @@ import {Component,OnInit} from '@angular/core';
 import {PostService} from './services/post.services';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {Post} from './services/post';
+import {GitHubProfileComponent} from './github-profile.component';
 
 @Component({
     // element selector my-app
@@ -12,8 +13,10 @@ import {Post} from './services/post';
               <div *ngIf="isLoading">
                 <i class="fa fa-spinner fa-spin fa-3x"></i> 
               </div>
+              <h2>Exercise</h2>
+              <github-profile></github-profile>
               `,
-            
+   directives:[GitHubProfileComponent],         
    providers:[PostService,HTTP_PROVIDERS]
 })
 
